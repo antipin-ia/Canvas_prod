78cd17641e61833864b207cd2548bc7a5c7af064
Antipin Ivan <antipinivan0491@gmail.com>
2025-06-29 17:40:19 +0300

    Updates

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventStoreService } from './services/event-store.service';
-import { CanvasGateway } from './gateways/canvas.gateway';
-
-@Module({
-  imports: [],
-  providers: [EventStoreService, CanvasGateway],
-})
-export class AppModule {} 
